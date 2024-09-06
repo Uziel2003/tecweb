60ba72b4e7cfee98731fb6940bcf8364e6c221c3
Uzi <abner.navez@alumno.buap.mx>
2024-09-06 16:36:49 -0600

    Se agrego el ejercicio 5 a la practica 5

---
+++ practicas/p05/ejercicio5.php
@@ -0,0 +1,10 @@
+<?php
+$a = "7 personas";
+$b = (integer) $a;
+$a = "9E3";
+$c = (double) $a;
+
+echo "\$a: $a<br>";  // Imprime "9E3"
+echo "\$b: $b<br>";  // Imprime 7
+echo "\$c: $c<br>";  // Imprime 9000
+?>
