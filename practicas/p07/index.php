2f066cd8615aa335bdbeeee9c329734312336d8c
Uzi <abner.navez@alumno.buap.mx>
2024-09-10 20:43:14 -0600

    Se actualizo index con el ejercicio 1 a la practica 7

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -1,30 +1,32 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+include 'src/funciones.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Práctica 4</title>
+    <title>Práctica 7</title>
 </head>
 <body>
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
-        if(isset($_GET['numero']))
-        {
-            $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
-        }
+    // Revisamos si el parámetro 'numero' ha sido pasado por la URL
+    if (isset($_GET['numero'])) {
+        $numero = $_GET['numero'];
+        $numero = intval($numero);
+        // Llamamos a la función esMultiploDe5y7 y mostramos el resultado
+        echo esMultiploDe5y7($numero);
+    } else {
+        echo "Por favor, pasa un número como parámetro en la URL. Ejemplo: ?numero=35";
+    }
     ?>
 
     <h2>Ejemplo de POST</h2>
-    <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
+    <form action="http://localhost/tecweb/practicas/p05/index.html" method="post">
         Name: <input type="text" name="name"><br>
         E-mail: <input type="text" name="email"><br>
         <input type="submit">
